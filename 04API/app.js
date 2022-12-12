4cb7808a0c050c6af84f1379cf07e4de9301fa4c
jam65st <jam65st@gmail.com>
2022-12-12 17:43:26 -0500

    fix: (04API) convenient relocation

--- 04API/app.js
+++ 04API/app.js
@@ -6,19 +6,24 @@
 
 const express      = require( 'express' ),
       path         = require( 'path' ),
+      // bodyParser   = require( 'body-parser' ),
       cookieParser = require( 'cookie-parser' ),
       logger       = require( 'morgan' ),
-      indexRouter  = require( './routes/index' ),
-      usersRouter  = require( './routes/users' ),
+      cors         = require( 'cors' ),
+      indexRouter  = require( './application/routes/index' ),
+      usersRouter  = require( './application/routes/users' ),
+      apiRouter    = require( './application/routes/api' ),
       app          = express();
 
 app.use( logger( 'dev' ) );
 app.use( express.json() );
 app.use( express.urlencoded( { extended: false } ) );
+app.use( cors() );
 app.use( cookieParser() );
 app.use( express.static( path.join( __dirname, 'public' ) ) );
 
 app.use( '/', indexRouter );
 app.use( '/users', usersRouter );
+app.use( '/api', apiRouter );
 
 module.exports = app;
